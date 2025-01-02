2ae3369b579e761291ad558145fd270f2448eed6
vippium <93650504+vippium@users.noreply.github.com>
2025-01-02 17:58:30 +0530

    Renamed folder

--- C Programming/Program to print simple words/HelloStudents.c
+++
@@ -1,7 +0,0 @@
-// Write a program to print "Hello Students" on screen
-
-#include <stdio.h>
-
-int main() {
-    printf("Hello Students");
-}
--- C Programming/Program to print the value of variable/Print_Variable_Value.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-
-int a = 12;
-printf("%d", a);
-
-}
